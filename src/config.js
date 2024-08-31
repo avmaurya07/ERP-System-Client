a8f6f495afcb0c55508ec6e37776e9d52f41d6b9
avmaurya07 <avmaurya07@gmail.com>
2024-08-31 14:34:53 +0530

    updation for hosting on server

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 
 const config =  {
-    // host : "http://192.168.21.66:5000",
-    host : "https://x404413m-5000.inc1.devtunnels.ms",
+    host : "http://localhost:5000",
 }
 
 export default config
